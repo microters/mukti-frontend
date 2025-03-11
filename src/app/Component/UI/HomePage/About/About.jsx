4422e3e36b71c02a30a5d417306a181f44f646bd
MicrotersAsad <sheblumicroters@gmail.com>
2025-03-11 13:13:53 +0600

    Add "use client" directive to About and Blog components

--- src/app/Component/UI/HomePage/About/About.jsx
+++ src/app/Component/UI/HomePage/About/About.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import Image from "next/image";
 import SectionHeading from "@/app/Component/Shared/SectionHeading/SectionHeading";
--- src/app/Component/UI/HomePage/Blog/Blog.jsx
+++ src/app/Component/UI/HomePage/Blog/Blog.jsx
@@ -1,3 +1,4 @@
+"use client"
 import SectionHeading from "@/app/Component/Shared/SectionHeading/SectionHeading";
 import { Icon } from "@iconify/react";
 import Image from "next/image";
