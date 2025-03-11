bb9de0f925a8ac417ace9efc759cb06bcb92503e
codershawon <shawonb500@gmail.com>
2025-03-11 16:29:44 +0530

    test about page

--- src/app/(withCommonLayout)/about/page.jsx
+++ src/app/(withCommonLayout)/about/page.jsx
@@ -1,30 +1,42 @@
-// app/about/page.js
+// // app/about/page.js
 
-import CommonHero from "@/app/Component/UI/CommonHero";
-import About from "@/app/Component/UI/HomePage/About/About";
-import Appointment from "@/app/Component/Shared/AppointmentAreas/Appointment";
-import WhyChooseUs from "@/app/Component/UI/HomePage/WhyChooseUs/WhyChooseUs";
-import AppointmentProcess from "@/app/Component/UI/HomePage/AppointmentProcess/AppointmentProcess";
-import Testimonials from "@/app/Component/UI/HomePage/Testimonials/Testimonials";
-import WhoWeAre from "@/app/Component/UI/WhoWeAre";
-import { getReviews } from "@/app/api/Reviews/Reviews";
+// import CommonHero from "@/app/Component/UI/CommonHero";
+// import About from "@/app/Component/UI/HomePage/About/About";
+// import Appointment from "@/app/Component/Shared/AppointmentAreas/Appointment";
+// import WhyChooseUs from "@/app/Component/UI/HomePage/WhyChooseUs/WhyChooseUs";
+// import AppointmentProcess from "@/app/Component/UI/HomePage/AppointmentProcess/AppointmentProcess";
+// import Testimonials from "@/app/Component/UI/HomePage/Testimonials/Testimonials";
+// import WhoWeAre from "@/app/Component/UI/WhoWeAre";
+// import { getReviews } from "@/app/api/Reviews/Reviews";
 
-// This is a Server Component. It can fetch data on the server before rendering the page.
-export default async function AboutUs() {
-  // Fetch reviews dynamically from API on the server side
-  const reviews = await getReviews();
+// // This is a Server Component. It can fetch data on the server before rendering the page.
+// export default async function AboutUs() {
+//   // Fetch reviews dynamically from API on the server side
+//   const reviews = await getReviews();
+//   console.log("Reviews", reviews)
 
-  // Return the page structure with the fetched reviews
+//   // Return the page structure with the fetched reviews
+//   return (
+//     <div>
+//       <CommonHero pageName="About Us" />
+//       <About />
+//       <Appointment />
+//       <WhoWeAre />
+//       <WhyChooseUs />
+//       <AppointmentProcess />
+//       <Testimonials reviews={reviews} />
+//     </div>
+//   );
+// }
+
+import React from 'react';
+
+const About = () => {
   return (
     <div>
-      <CommonHero pageName="About Us" />
-      <About />
-      <Appointment />
-      <WhoWeAre />
-      <WhyChooseUs />
-      <AppointmentProcess />
-      <Testimonials reviews={reviews} /> {/* Pass reviews as props */}
+      <p>Hello Riaz</p>
     </div>
   );
-}
+};
 
+export default About;
