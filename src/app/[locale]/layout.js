58e70ea400f7d1d9fbf19913f6e715ebff9f5639
codershawon <shawonb500@gmail.com>
2025-03-11 16:51:13 +0530

    in app folder page.jsx file created

--- src/app/[locale]/layout.js
+++
@@ -1,8 +0,0 @@
-export default function LocaleLayout({ children, params }) {
-    return (
-      <html lang={params.locale}>
-        <body>{children}</body>
-      </html>
-    );
-  }
-  
--- src/app/[locale]/page.js
+++
@@ -1,9 +0,0 @@
-// Language change file
-export default function LocaleHome({ params }) {
-    return (
-      <div>
-        <h1>{params.locale === "bn" ? "বাংলা হোম পেজ" : "English Home Page"}</h1>
-      </div>
-    );
-  }
-  
