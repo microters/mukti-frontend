327e932a90d0ec37aaaaf4552fce0d2604e77361
codershawon <shawonb500@gmail.com>
2025-03-12 11:18:11 +0530

    category section image updated

--- src/app/Component/UI/HomePage/Category/Category.jsx
+++ src/app/Component/UI/HomePage/Category/Category.jsx
@@ -26,36 +26,41 @@ const Category = ({ departments }) => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-6 mt-10 mb-10">
           {/* Mapping over departments */}
-          {departments.map((department) => (
-            <div key={department.id} className="bg-white group text-center py-8 px-6 rounded-lg overflow-hidden relative">
-              <Image
-                src={shape}
-                alt="Shape"
-                className="absolute -left-3 top-0 max-w-36 opacity-0 group-hover:opacity-100 group-hover:left-0 transition-all duration-300"
-              />
-              <div className="flex items-center justify-center size-20 mx-auto mb-3">
+          {departments.map((department) => {
+            const departmentIcon = department.icon
+            ? `${process.env.NEXT_PUBLIC_BACKEND_URL}${department.icon}`
+            : "/default-profile-photo.png";
+            return (
+              <div key={department.id} className="bg-white group text-center py-8 px-6 rounded-lg overflow-hidden relative">
                 <Image
-                  src={department.icon}
-                  alt={department.translations[currentLanguage]?.name || "Department"}
-                  width={80}
-                  height={80}
-                  className="object-cover"
-                  unoptimized
+                  src={shape}
+                  alt="Shape"
+                  className="absolute -left-3 top-0 max-w-36 opacity-0 group-hover:opacity-100 group-hover:left-0 transition-all duration-300"
                 />
+                <div className="flex items-center justify-center size-20 mx-auto mb-3">
+                  <Image
+                    src={departmentIcon}
+                    alt={department.translations[currentLanguage]?.name || "Department"}
+                    width={80}
+                    height={80}
+                    className="object-cover"
+                    unoptimized
+                  />
+                </div>
+                {/* Department Name */}
+                <h3 className="text-xl text-M-heading-color font-bold font-jost">
+                  {department.translations[currentLanguage]?.name || "Department"}
+                </h3>
+  
+                <Link
+                  href={`/${currentLanguage === 'bn' ? 'bn/' : ''}departments/${department.id}`}
+                  className="size-14 inline-flex items-center justify-center bg-[#E6F5F3] text-M-primary-color rounded-full mt-5 origin-center transition-all duration-300 group-hover:bg-M-secondary-color group-hover:text-white"
+                >
+                  <Icon icon="solar:arrow-right-linear" width={24} height={24} />
+                </Link>
               </div>
-              {/* Department Name */}
-              <h3 className="text-xl text-M-heading-color font-bold font-jost">
-                {department.translations[currentLanguage]?.name || "Department"}
-              </h3>
-
-              <Link
-                href={`/${currentLanguage === 'bn' ? 'bn/' : ''}departments/${department.id}`}
-                className="size-14 inline-flex items-center justify-center bg-[#E6F5F3] text-M-primary-color rounded-full mt-5 origin-center transition-all duration-300 group-hover:bg-M-secondary-color group-hover:text-white"
-              >
-                <Icon icon="solar:arrow-right-linear" width={24} height={24} />
-              </Link>
-            </div>
-          ))}
+            )
+          })}
         </div>
 
         {/* Button */}
