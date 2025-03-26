fb5b95b62023412f8f97586c4cc786b3baaeec4c
codershawon <shawonb500@gmail.com>
2025-03-26 11:09:50 +0530

    header,footer updated

--- src/app/api/dynamicData,.js
+++ src/app/api/dynamicData,.js
@@ -5,10 +5,33 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
+    "x-api-key": process.env.NEXT_PUBLIC_API_KEY,
     "Content-Type": "application/json",
   },
   cache: "no-store"
 });
+
+// Fetch Header Data
+export const fetchHeaderData = async (language = 'en') => {
+  try {
+    const response = await apiClient.get(`api/header?lang=${language}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+// Fetch Footer Data
+export const fetchFooterData = async (language = 'en') => {
+  try {
+    const response = await apiClient.get(`api/footer?lang=${language}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
 
 // ✅ Fetch all dynamic home page data
 export const fetchDynamicData = async (language = 'en') => {
