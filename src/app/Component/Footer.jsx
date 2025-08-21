c4bf0a577010aae6ec0508c4a210bff963a6ee9e
codershawon <shawonb500@gmail.com>
2025-08-21 16:45:19 +0530

    Refactor Footer and FooterProvider components to improve data handling and add polling for updates

--- src/app/Component/Footer.jsx
+++ src/app/Component/Footer.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { fetchDoctors } from '../api/doctor';
-import FooterProvider from './FooterProvider/FooterProvider';
-import { fetchFooterData } from '../api/dynamicData,';
+// import React from 'react';
+// import { fetchDoctors } from '../api/doctor';
+// import FooterProvider from './FooterProvider/FooterProvider';
+// import { fetchFooterData } from '../api/dynamicData,';
 
-const Footer = async() => {
+// const Footer = async() => {
+//   const doctors = await fetchDoctors();
+//   const footer = await fetchFooterData()
+//   return <FooterProvider doctors={doctors} footer={footer}/>;
+// };
+
+// export default Footer;
+
+import { fetchDoctors } from "../api/doctor";
+import { fetchFooterData } from "../api/dynamicData,";
+import FooterProvider from "./FooterProvider/FooterProvider";
+
+const Footer = async () => {
   const doctors = await fetchDoctors();
-  const footer = await fetchFooterData()
-  return <FooterProvider doctors={doctors} footer={footer}/>;
+  const footer = await fetchFooterData();
+
+  return <FooterProvider initialDoctors={doctors} initialFooter={footer} />;
 };
 
 export default Footer;
-
